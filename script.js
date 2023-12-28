3d731ab0bb7a51f00c09348d4443efe412d37838
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2023-12-28 08:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function isSameType(value1, value2) {
   }
 
   // Check if the types are the same
-  return typeof value1 === typeof value2;
+  return typeof value1 === typeof value2 && !isNaN(value1) && !isNaN(value2) ;
 }
 
 // do not change the code below.
